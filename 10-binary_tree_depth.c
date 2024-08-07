55059036c242cb357d51b6121ff3d6d6a474f4eb
Mirheybet <mirheybet.hesenov@gmail.com>
2024-08-07 21:47:50 +0000

    task10-depthofbinarytree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+#include <stddef.h>
+/**
+ * binary_tree_depth - main
+ * @tree: clone of main tree
+ * Return: depth of node
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	const binary_tree_t *clonetree = tree;
+	
+	int depth = 0;
+
+	if (clonetree == NULL || tree == NULL)
+	{
+		return (0);
+	}
+
+	while (clonetree->parent != NULL)
+	{
+		depth++;
+		clonetree = clonetree->parent;
+	}
+
+	return (depth);
+}
