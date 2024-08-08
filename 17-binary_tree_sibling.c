71fb295b64d22ccd460b87e30fe0eeacee2dd15f
MirHeybet <mirheybet.hesenov@gmail.com>
2024-08-08 19:23:54 +0000

    task17-siblingtree-upd1

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,27 +2,28 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_sibling - main
+ * @node: node
+ * Return: sibling
  */
 
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 
-	if(node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
 
 	binary_tree_t *clone = node->parent;
-	if(clone->right == node)
+
+	if (clone->right == node)
 	{
 		return (clone->left);
 	}
 
-	if(clone->left == node)
+	if (clone->left == node)
 	{
 		return (clone->right);
 	}
